ba7ffb65bec7aba0c4b144448530699b227a88ab
John Hoover <defvayne23@hardy2.(none)>
2009-09-20 04:13:12 +0000

    Start work on generic form submit

--- controllers/content.controller.php
+++ controllers/content.controller.php
@@ -48,6 +48,59 @@ class content extends AppController
 		else
 			$this->error("404");
 	}
+	function form_submit()
+	{
+		// Simple bot checker, user with css turned off would probably not pass this check though
+		if(empty($_POST["subject"]))
+		{
+			$aComp = array();//Email components
+			$aInfo = array();//Form values
+			
+			// Build array from from, only those made for the email will do
+			foreach($_POST as $x => $input)
+			{
+				$info = explode("|",str_replace("_"," ",$x));
+				
+				// Check if made for email
+				if(count($info) > 1)
+				{
+					$aComp[$info[0]] = Array(
+						"linetype" => $info[1],
+						"name" => $info[2],
+						"value" => $input
+						);
+					$aInfo[$info[2]] = $input;
+				}
+			}
+			// Sort based on data in form
+			ksort($aComp);
+			
+			//Build email
+			$sBody = "";
+			foreach($aComp as $input)
+			{
+				// Only padding below
+				if($input["linetype"] == "s")
+					$sBody .= $input["name"]." ".stripslashes($input["value"])."\n";
+				// Padding on top and bottom
+				elseif($input["linetype"] == "n")
+					$sBody .= "\n".$input["name"]."\n".stripslashes($input["value"])."\n";
+			}
+			
+			// Email to
+			$aRecipients = array(
+				$this->decrypt($_POST["to"])
+			);
+			
+			$aHeaders["From"] = $this->decrypt($_POST["from"]);
+			$aHeaders["To"] = $this->decrypt($_POST["to"]);
+			$aHeaders["Subject"] = $this->decrypt($_POST["subject"]);
+			
+			$this->mail($aRecipients, $aHeaders, $sBody);
+		}
+		
+		$this->forward($this->decrypt($_POST["forward"]));
+	}
 	##################################
 	
 	### Functions ####################
