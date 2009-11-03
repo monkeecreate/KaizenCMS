2e4824bf224c67d7a0fe4402e7872293b73c0a7c
cranewest <cranewest@mail.crane-west.net>
2009-11-02 22:39:11 -0600

    Update component getContent to use tags

--- components/function.getContent.php
+++ components/function.getContent.php
@@ -3,7 +3,13 @@ function smarty_function_getContent($aParams, &$oSmarty)
 {
 	$objDB = $oSmarty->get_registered_object("objDB");
 	
-	$sContent = $objDB->query("SELECT `content` FROM `content` WHERE `id` = ".$objDB->quote($aParams["id"], "integer"))->fetchOne();
+	if(!empty($aParams["tag"]))
+		$aContent = $objDB->query("SELECT * FROM `content` WHERE `tag` = ".$objDB->quote($aParams["tag"], "text"))->fetchRow();
+	elseif(!empty($aParams["id"]))
+		$aContent = $objDB->query("SELECT * FROM `content` WHERE `id` = ".$objDB->quote($aParams["id"], "integer"))->fetchRow();
 	
-	return $sContent;
+	if(empty($aParams["var"]))
+		return stripslashes($aContent["content"]);
+	else
+		$oSmarty->assign($aParams["var"], $aContent);
 }
