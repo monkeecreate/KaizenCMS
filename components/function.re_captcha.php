4a0caa35fdeacfc6d1a26103891a43590796b28c
John Hoover <defvayne23@hardy2.(none)>
2009-11-23 12:20:37 -0600

    Add files for forms

---
+++ components/function.re_captcha.php
@@ -0,0 +1,10 @@
+<?php
+function smarty_function_re_captcha($aParams, &$oSmarty)
+{
+	$oApp = $oSmarty->get_registered_object("appController");
+	
+	require_once($oApp->_settings->root.'helpers/recaptchalib.php');
+	$publickey = "6LfXQwkAAAAAAAlpqjlApE8ZsoW2vu5w4Cm-BlBe"; // you got this from the signup page
+	echo recaptcha_get_html($publickey);
+
+}
