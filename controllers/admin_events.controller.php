7f299b110e4589f9bacbd2f4dcf535bb0d500712
fleeting <twofivethreetwo@gmail.com>
2010-01-08 14:12:49 -0600

    event bug fixes (fixed POST['id'] and added minWidth/Height from events model) // throw error on small image upload

--- controllers/admin_events.controller.php
+++ controllers/admin_events.controller.php
@@ -299,6 +299,8 @@ class admin_events extends adminController
 	}
 	function image_upload($aParams)
 	{
+		$oEvents = $this->loadModel("events");
+		
 		$aEvent = $this->db_results(
 			"SELECT * FROM `events`"
 				." WHERE `id` = ".$this->db_quote($aParams["id"], "integer")
@@ -307,11 +309,13 @@ class admin_events extends adminController
 		);
 
 		$this->tpl_assign("aEvent", $aEvent);
-
+		$this->tpl_assign("minWidth", $oEvents->imageMinWidth);
+		$this->tpl_assign("minHeight", $oEvents->imageMinHeight);
 		$this->tpl_display("events/image/upload.tpl");
 	}
 	function image_upload_s()
 	{
+		$oEvents = $this->loadModel("events");
 		$folder = $this->_settings->root_public."uploads/events/";
 		
 		if(!is_dir($folder))
@@ -326,19 +330,25 @@ class admin_events extends adminController
 
 			if(move_uploaded_file($_FILES["image"]["tmp_name"], $folder.$_POST["id"].".jpg"))
 			{
-				$this->db_results(
-					"UPDATE `events` SET"
-						." `photo_x1` = 0"
-						.", `photo_y1` = 0"
-						.", `photo_x2` = 194"
-						.", `photo_y2` = 129"
-						.", `photo_width` = 194"
-						.", `photo_height` = 129"
-						." WHERE `id` = ".$_POST["id"]
-					,"admin->events->image->upload"
-				);
+				$aImageSize = getimagesize($folder.$_POST["id"].".jpg");
+				if($aImageSize[0] < $oEvents->imageMinWidth || $aImageSize[1] < $oEvents->imageMinHeight) {
+					@unlink($folder + $id.".jpg");
+					$this->forward("/admin/events/image/".$_POST["id"]."/upload/?error=".urlencode("Image does not meet the minimum width and height requirements."));
+				} else {
+					$this->db_results(
+						"UPDATE `events` SET"
+							." `photo_x1` = 0"
+							.", `photo_y1` = 0"
+							.", `photo_x2` = 194"
+							.", `photo_y2` = 129"
+							.", `photo_width` = 194"
+							.", `photo_height` = 129"
+							." WHERE `id` = ".$_POST["id"]
+						,"admin->events->image->upload"
+					);
 
-				$this->forward("/admin/events/image/".$_POST["id"]."/edit/");
+					$this->forward("/admin/events/image/".$_POST["id"]."/edit/");
+				}
 			}
 			else
 				$this->forward("/admin/events/image/".$_POST["id"]."/upload/?error=".urlencode("Unable to upload image."));
--- views/admin/events/image/upload.tpl
+++ views/admin/events/image/upload.tpl
@@ -2,18 +2,18 @@
 <div id="sidebar" class="portlet">
 	<div class="portlet-content">
 		<b>&middot;</b> File must be a .jpg<br />
-		<b>&middot;</b> Minimum width is 300px<br />
-		<b>&middot;</b> Minimum height is 225px
+		<b>&middot;</b> Minimum width is {$minWidth}px<br />
+		<b>&middot;</b> Minimum height is {$minHeight}px
 	</div>
 </div>
 <p>
 	<b>{$aArticle.title|stripslashes}</b><br />
 	<span>{event_time allday=$aEvent.allday start=$aEvent.datetime_start end=$aEvent.datetime_end}</span>
 </p><br />
-<form name="upload" action="/admin/news/image/upload/s/" method="post" enctype="multipart/form-data">
+<form name="upload" action="/admin/events/image/upload/s/" method="post" enctype="multipart/form-data">
 	<label>Choose File:</label>
 	<input type="file" name="image" /><br />
 	<input type="submit" value="Upload File" /> <input type="button" value="Cancel" onclick="history.back(-1);" />
-	<input type="hidden" name="id" value="{$aArticle.id}">
+	<input type="hidden" name="id" value="{$aEvent.id}">
 </form>
 {include file="inc_footer.tpl"}
