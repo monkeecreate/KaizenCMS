217b15863097f36cd2da8175b40cf8eb256e4fa3
John Hoover <defvayne23@gmail.com>
2010-06-26 23:06:11 -0500

    Change documents plugin to the new prepared statements method

--- plugins/documents/controllers/admin_documents.php
+++ plugins/documents/controllers/admin_documents.php
@@ -25,7 +25,6 @@ class admin_documents extends adminController
 		
 		if(!empty($_SESSION["admin"]["admin_documents"]))
 			$this->tplAssign("aDocument", $_SESSION["admin"]["admin_documents"]);
-		
 		else
 			$this->tplAssign("aDocument",
 				array(
@@ -46,36 +45,36 @@ class admin_documents extends adminController
 		}
 		
 		$sID = $this->dbQuery(
-			"INSERT INTO `{dbPrefix}documents`"
-				." (`name`, `description`, `active`, `created_datetime`, `created_by`, `updated_datetime`, `updated_by`)"
-				." VALUES"
-				." ("
-					.$this->dbQuote($_POST["name"], "text")
-					.", ".$this->dbQuote($_POST["description"], "text")
-					.", ".$this->boolCheck($_POST["active"])
-					.", ".$this->dbQuote(time(), "integer")
-					.", ".$this->dbQuote($_SESSION["admin"]["userid"], "integer")
-					.", ".$this->dbQuote(time(), "integer")
-					.", ".$this->dbQuote($_SESSION["admin"]["userid"], "integer")
-				.")"
-			,"insert"
+			"documents",
+			array(
+				"name" => $_POST["name"]
+				,"description" => $_POST["description"]
+				,"active" => $this->boolCheck($_POST["active"])
+				,"created_datetime" => time()
+				,"created_by" => $_SESSION["admin"]["userid"]
+				,"updated_datetime" => time()
+				,"updated_by" => $_SESSION["admin"]["userid"]
+			)
 		);
 		
 		foreach($_POST["categories"] as $sCategory) {
-			$this->dbQuery(
-				"INSERT INTO `{dbPrefix}documents_categories_assign`"
-					." (`documentid`, `categoryid`)"
-					." VALUES"
-					." (".$sID.", ".$sCategory.")"
+			$this->dbInsert(
+				"documents_categories_assign",
+				array(
+					"documentid" => $sID,
+					"categoryid" => $sCategory
+				)
 			);
 		}
 		
 		if(!empty($_FILES["document"]["name"])) {
 			if($_FILES["document"]["error"] == 1) {
-				$this->dbQuery(
-					"UPDATE `{dbPrefix}documents` SET"
-						." `active` = 0"
-						." WHERE `id` = ".$this->dbQuote($sID, "integer")
+				$this->dbUpdate(
+					"documents",
+					array(
+						"active" => 0
+					),
+					$sID
 				);
 				
 				$this->forward("/admin/document/?notice=".urlencode("Document file size was too large!"));
@@ -90,16 +89,20 @@ class admin_documents extends adminController
 				
 				if(in_array($file_ext, $oDocuments->allowedExt) || empty($oDocuments->allowedExt)) {
 					if(move_uploaded_file($_FILES["document"]["tmp_name"], $upload_dir.$upload_file)) {
-						$this->dbQuery(
-							"UPDATE `{dbPrefix}documents` SET"
-								." `document` = ".$this->dbQuote($upload_file, "text")
-								." WHERE `id` = ".$this->dbQuote($sID, "integer")
+						$this->dbUpdate(
+							"documents",
+							array(
+								"document" => $upload_file
+							),
+							$sID
 						);
 					} else {
-						$this->dbQuery(
-							"UPDATE `{dbPrefix}documents` SET"
-								." `active` = 0"
-								." WHERE `id` = ".$this->dbQuote($sID, "integer")
+						$this->dbUpdate(
+							"documents",
+							array(
+								"active" => 0
+							),
+							$sID
 						);
 						
 						$this->forward("/admin/documents/edit/".$sID."/?error=".urlencode("Failed to upload file!"));
@@ -166,35 +169,37 @@ class admin_documents extends adminController
 			$this->forward("/admin/documents/edit/".$_POST["id"]."/?error=".urlencode("Please fill in all required fields!"));
 		}
 		
-		$this->dbQuery(
-			"UPDATE `{dbPrefix}documents` SET"
-				." `name` = ".$this->dbQuote($_POST["name"], "text")
-				.", `description` = ".$this->dbQuote($_POST["description"], "text")
-				.", `active` = ".$this->boolCheck($_POST["active"], "integer")
-				.", `updated_datetime` = ".$this->dbQuote(time(), "integer")
-				.", `updated_by` = ".$this->dbQuote($_SESSION["admin"]["userid"], "integer")
-				." WHERE `id` = ".$this->dbQuote($_POST["id"], "integer")
+		$this->dbUpdate(
+			"documents",
+			array(
+				"name" => $_POST["name"]
+				,"description" => $_POST["description"]
+				,"active" => $this->boolCheck($_POST["active"])
+				,"updated_datetime" => time()
+				,"updated_by" =>$_SESSION["admin"]["userid"]
+			)
+			$_POST["id"]
 		);
 		
-		$this->dbQuery(
-			"DELETE FROM `{dbPrefix}documents_categories_assign`"
-				." WHERE `documentid` = ".$this->dbQuote($_POST["id"], "integer")
-		);
+		$this->dbDelete("documents_categories_assign", $_POST["id"], "documentid");
 		foreach($_POST["categories"] as $sCategory) {
-			$this->dbQuery(
-				"INSERT INTO `{dbPrefix}documents_categories_assign`"
-					." (`documentid`, `categoryid`)"
-					." VALUES"
-					." (".$this->dbQuote($_POST["id"], "integer").", ".$sCategory.")"
+			$this->dbInsert(
+				"documents_categories_assign",
+				array(
+					"documentid" => $_POST["id"],
+					"categoryid" => $sCategory
+				)
 			);
 		}
 		
 		if(!empty($_FILES["document"]["name"])) {
 			if($_FILES["document"]["error"] == 1) {
-				$this->dbQuery(
-					"UPDATE `{dbPrefix}documents` SET"
-						." `active` = 0"
-						." WHERE `id` = ".$this->dbQuote($_POST["id"], "integer")
+				$this->dbUpdate(
+					"documents",
+					array(
+						"active" => 0
+					),
+					$_POST["id"]
 				);
 				
 				$this->forward("/admin/documents/?notice=".urlencode("Document file size was too large!"));
@@ -216,16 +221,20 @@ class admin_documents extends adminController
 					@unlink($upload_dir.$sDocument);
 			
 					if(move_uploaded_file($_FILES["document"]["tmp_name"], $upload_dir.$upload_file)) {
-						$this->dbQuery(
-							"UPDATE `{dbPrefix}documents` SET"
-								." `document` = ".$this->dbQuote($upload_file, "text")
-								." WHERE `id` = ".$this->dbQuote($_POST["id"], "integer")
+						$this->dbUpdate(
+							"documents",
+							array(
+								"document" => $upload_file
+							),
+							$_POST["id"]
 						);
 					} else {
-						$this->dbQuery(
-							"UPDATE `{dbPrefix}documents` SET"
-								." `active` = 0"
-								." WHERE `id` = ".$this->dbQuote($_POST["id"], "integer")
+						$this->dbUpdate(
+							"documents",
+							array(
+								"active" => 0
+							),
+							$_POST["id"]
 						);
 					
 						$this->forward("/admin/documents/?notice=".urlencode("Failed to upload file!"));
@@ -246,14 +255,8 @@ class admin_documents extends adminController
 		
 		@unlink($this->_settings->rootPublic.substr($oDocuments->documentFolder, 1).$aDocument["document"]);
 		
-		$this->dbQuery(
-			"DELETE FROM `{dbPrefix}documents`"
-				." WHERE `id` = ".$this->dbQuote($this->_urlVars->dynamic["id"], "integer")
-		);
-		$this->dbQuery(
-			"DELETE FROM `{dbPrefix}documents_categories_assign`"
-				." WHERE `documentid` = ".$this->dbQuote($this->_urlVars->dynamic["id"], "integer")
-		);
+		$this->dbDelete("documents", $this->_urlVars->dynamic["id"]);
+		$this->dbDelete("documents_categories_assign", $this->_urlVars->dynamic["id"], "documentid");
 		
 		$this->forward("/admin/documents/?notice=".urlencode("Document removed successfully!"));
 	}
@@ -262,47 +265,34 @@ class admin_documents extends adminController
 		
 		$_SESSION["admin"]["admin_documents_categories"] = null;
 		
-		$aCategories = $this->dbQuery(
-			"SELECT * FROM `{dbPrefix}documents_categories`"
-				." ORDER BY `name`"
-			,"all"
-		);
-		
-		$this->tplAssign("aCategories", $aCategories);
+		$this->tplAssign("aCategories", $oDocuments->getCategories());
 		$this->tplAssign("aCategoryEdit", $oDocuments->getCategory($_GET["category"]));
 		$this->tplDisplay("admin/categories.tpl");
 	}
 	function categories_add_s() {
-		$this->dbQuery(
-			"INSERT INTO `{dbPrefix}documents_categories`"
-				." (`name`)"
-				." VALUES"
-				." ("
-				.$this->dbQuote($_POST["name"], "text")
-				.")"
-			,"insert"
+		$this->dbInsert(
+			"documents_categories",
+			array(
+				"name" => $_POST["name"]
+			)
 		);
 
 		$this->forward("/admin/documents/categories/?notice=".urlencode("Category created successfully!"));
 	}
 	function categories_edit_s() {
-		$this->dbQuery(
-			"UPDATE `{dbPrefix}documents_categories` SET"
-				." `name` = ".$this->dbQuote($_POST["name"], "text")
-				." WHERE `id` = ".$this->dbQuote($_POST["id"], "integer")
+		$this->dbUpdate(
+			"documents_categories",
+			array(
+				"name" => $_POST["name"]
+			),
+			$_POST["id"]
 		);
 
 		$this->forward("/admin/documents/categories/?notice=".urlencode("Changes saved successfully!"));
 	}
 	function categories_delete() {
-		$this->dbQuery(
-			"DELETE FROM `{dbPrefix}documents_categories`"
-				." WHERE `id` = ".$this->dbQuote($this->_urlVars->dynamic["id"], "integer")
-		);
-		$this->dbQuery(
-			"DELETE FROM `{dbPrefix}documents_categories_assign`"
-				." WHERE `categoryid` = ".$this->dbQuote($this->_urlVars->dynamic["id"], "integer")
-		);
+		$this->dbDelete("documents_categories", $this->_urlVars->dynamic["id"]);
+		$this->dbDelete("documents_categories_assign", $this->_urlVars->dynamic["id"], "categoryid");
 
 		$this->forward("/admin/documents/categories/?notice=".urlencode("Category removed successfully!"));
 	}
