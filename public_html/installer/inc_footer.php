0d988f7958df4ec76c284aaa17fdd6b9df260aa6
fleeting <twofivethreetwo@gmail.com>
2010-08-23 16:00:51 -0500

    cleaning up the installer, adding form validation

--- public_html/installer/inc_footer.php
+++ public_html/installer/inc_footer.php
@@ -1,11 +1,10 @@
 		</section> <!-- #content -->
-		
+	
 		<footer>
 			<p></p>
 		</footer>
 	</div> <!-- #wrapper -->
-	
-	<script src="/scripts/jquery-1.4.2.min.js"></script>
+
 	<script>
 	$(document).ready(function() {
 		$(".showAdvanced").click(function() {
@@ -19,6 +18,38 @@
 			$("#"+$(this).attr("value")).slideDown("slow");
 		});
 	});
+	</script>
+	<script>
+	(function($) {
+		jQuery.fn.validateForm = function(fields){
+			if(fields == '' || fields == null)
+				fields = new Array;
+
+			this.each(function(){
+				$(this).RSV({
+					errorFieldClass: "errorField",
+					customErrorHandler: function(f, errorInfo) {
+						//console.log(errorInfo);
+						if(errorInfo != 0) {
+							$("#formErrors").html('');
+							$("#formErrors").prepend('<ul></ul>');
+							for (var i=0; i<errorInfo.length; i++) {
+								$('#formErrors ul').append('<li><span class="iconic fail">x</span> '+errorInfo[i][1]+'</li>');
+								$(errorInfo[i][0]).addClass("errorField");
+							}
+
+							errorInfo[0][0].focus();
+							$('html, body').animate({scrollTop:0}, 'slow');
+
+							return false;
+						} else
+							return true;
+					},
+					rules: fields
+				});
+			});
+		};
+	})(jQuery);
 	</script>
 	<!--[if lt IE 7 ]>
 		<script src="/scripts/dd_belatedpng.js?v=1"></script>
