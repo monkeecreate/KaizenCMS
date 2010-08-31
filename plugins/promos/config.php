6fd1eabe4a506648117a934642bc4fd77864d703
James Fleeting <twofivethreetwo@gmail.com>
2010-08-31 13:11:57 -0500

    removed plugins, they will be a different repo, added .empty

--- plugins/promos/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$aPluginInfo = array(
-	"name" => "Promos",
-	"version" => "1.0",
-	"author" => "monkeeCreate",
-	"website" => "http://monkeecreate.com/",
-	"email" => "support@monkeecreate.com"
-);
