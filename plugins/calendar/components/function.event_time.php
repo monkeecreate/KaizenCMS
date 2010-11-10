de1b6c9aac32cd4cad8d8f12d33a32c97ee04c8a
John Hoover <defvayne23@gmail.com>
2010-11-09 22:56:26 -0600

    Do not show a time range when start and end dates/time are the same, and the event is not all day. Also added comments to describe what is happening in the Smarty function.

--- plugins/calendar/components/function.event_time.php
+++ plugins/calendar/components/function.event_time.php
@@ -1,14 +1,39 @@
 <?php
 function smarty_function_event_time($aParams, &$oSmarty) {
 	if($aParams["allday"] == 1) {
-		if(date("d", $aParams["start"]) != date("d", $aParams["end"]))
+		// Event is all day, don't ever show the time
+		
+		if(date("d", $aParams["start"]) != date("d", $aParams["end"])) {
+			// Start and end dates are different
+			// Show a date range
+			
 			return date("m/d/y", $aParams["start"])." - ".date("m/d/y", $aParams["end"]);
-		else
+		} else {
+			// Start and end date are not different
+			// Show only the date
+			
 			return date("m/d/y", $aParams["start"]);
+		}
+	
 	} else {
-		if(date("d", $aParams["start"]) != date("d", $aParams["end"]))
+		// Event is not all day, so we need to figure out how to show the time
+		
+		if(date("d", $aParams["start"]) != date("d", $aParams["end"])) {
+			// Start and end dates are different
+			// Show two sets of date and time
+			
 			return date("m/d/y g:i a", $aParams["start"])." - ".date("m/d/y g:i a", $aParams["end"]);
-		else
-			return date("m/d/y g:i a", $aParams["start"])." - ".date("g:i a", $aParams["end"]);
+		} else {
+			// Start and end are not different
+			
+			$sDateString = date("m/d/y g:i a", $aParams["start"]);
+			
+			// If start and end times are different, add end time to create time range
+			if(date("g:i a", $aParams["start"]) == date("g:i a", $aParams["end"])) {
+				$sDateString .= " - ".date("g:i a", $aParams["end"]);
+			}
+			
+			return $sDateString;
+		}
 	}
 }
