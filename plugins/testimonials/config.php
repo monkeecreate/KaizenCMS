a605bdfea6e0df959ae5e002b1c15512686ff1f3
John Hoover <defvayne23@gmail.com>
2010-11-14 21:44:43 -0600

    Add sort setting to Testimonials.

--- plugins/testimonials/config.php
+++ plugins/testimonials/config.php
@@ -1,7 +1,7 @@
 <?php
 $aPluginInfo = array(
 	"name" => "Testimonials",
-	"version" => "1.0",
+	"version" => "1.1",
 	"author" => "monkeeCreate",
 	"website" => "http://monkeecreate.com/",
 	"email" => "support@monkeecreate.com"
