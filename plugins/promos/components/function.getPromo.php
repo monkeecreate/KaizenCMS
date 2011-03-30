86a9087329044f634c7b4908e0819cfae91bbfde
James Fleeting <twofivethreetwo@gmail.com>
2011-03-30 10:40:33 -0500

    fixed getPromo bug

--- plugins/promos/components/function.getPromo.php
+++ plugins/promos/components/function.getPromo.php
@@ -16,7 +16,7 @@ function smarty_function_getPromo($aParams, &$oSmarty) {
 		$aPosition = $oPromo->getPosition($aParams["tag"]);
 		
 		if(!empty($aPromo["link"]))
-			echo "<a href=\"/promos/".$aPromo["id"]."/\" class=\"".$aParams["tag"]" promo\">";
+			echo "<a href=\"/promos/".$aPromo["id"]."/\" class=\"".$aParams["tag"]." promo\">";
 		
 		echo "<img src=\"".$oPromo->imageFolder.$aPromo["promo"]."\" style=\"width:".$aPosition["promo_width"]."px;height:".$aPosition["promo_height"]."px;\" class=\"".$aParams["tag"]." promo\">";
 		
