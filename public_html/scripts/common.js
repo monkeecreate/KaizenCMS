67aaa987a2c66f6b4583e8940ca12fba38d77850
James Fleeting <twofivethreetwo@gmail.com>
2011-08-23 09:41:06 -0500

    cleaned up common js, removed code no longer needed

--- public_html/scripts/common.js
+++ public_html/scripts/common.js
@@ -1,18 +1,4 @@
-$(document).ready(function() {
-	/**
-	 * DIV Equal Height
-	 *
-	 * Any divs with equal heights will be given
-	 * the height of the tallest div.
-	 */
-	var equalHeight = 0;
-	$(".column").each(function(){
-		thisHeight = $(this).height();
-		if(thisHeight > equalHeight) {
-			equalHeight = thisHeight;
-		}
-	}).height(equalHeight);
-	
+$(document).ready(function() {	
 	/**
 	 * FAQ Answer Toggle
 	 *
@@ -27,53 +13,6 @@ $(document).ready(function() {
 		$(faqID).slideToggle(400);
 		$.scrollTo(this, 1000);
 		return false;
-	});
-	
-	/**
-	 * Form input classes
-	 * 
-	 * Applys classes to form items to easily apply
-	 * css to form items.
-	 */
-	$('input:text').addClass('inputText');
-	$('input:password').addClass('inputPassword');
-	$('input:checkbox').addClass('inputCheckbox');
-	$('input:radio').addClass('inputRadio');
-	$('textarea').addClass('textarea');
-	$('select').addClass('select');
-	$('input:submit').addClass('inputSubmit');
-	$('input:image').addClass('inputImage');
-	
-	/**
-	 * Default form text
-	 * 
-	 * Applys click event to clear default text on any
-	 * input with class .default. Also adds the default
-	 * value in the data object of the input so when
-	 * the form is submited, the default value is not
-	 * sent to the server.
-	 */
-	$('input.default, textarea.default').each(function() {
-		var input = $(this);
-		input.data("val", input.val());
-		input.focus(function() {
-			if(input.val() == input.data("val")) {
-				input.val("");
-				input.removeClass("default");
-			}
-		});
-		input.blur(function() {
-			if(input.val() == "") {
-				input.val(input.data("val"));
-				input.addClass("default");
-			}
-		});
-	});
-	$('form').submit(function() {
-		var form = $(this);
-		form.find('.default').each(function() {
-			$(this).val("");
-		})
 	});
 	
 	/**
