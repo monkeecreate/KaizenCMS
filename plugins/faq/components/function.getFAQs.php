7a8518a170591f082586550e35e4ffe217a852de
James Fleeting <twofivethreetwo@gmail.com>
2011-09-15 13:51:40 -0500

    cleaned up getFAQs

--- plugins/faq/components/function.getFAQs.php
+++ plugins/faq/components/function.getFAQs.php
@@ -1,21 +1,20 @@
 <?php
 function smarty_function_getFAQs($aParams, &$oSmarty) {
 	$oApp = $oSmarty->getRegisteredObject("appController");
+	$oQuestions = $oApp->loadModel("faq");
 	
-	$oFAQ = $oApp->loadModel("faq");
-	
-	$aArticlePages = array_chunk($oFAQ->getQuestions($aParams["category"]), $aParams["limit"]);
-	$aArticles = $aArticlePages[0];
+	$aQuestionPages = array_chunk($oQuestions->getQuestions($aParams["category"]), $aParams["limit"]);
+	$aQuestions = $aQuestionPages[0];
 	
 	if($aParams["limit"] == 1) {
 		if(!empty($aParams["assign"]))
-			$oSmarty->assign($aParams["assign"], $aArticles[0]);
+			$oSmarty->assign($aParams["assign"], $aQuestions[0]);
 		else
-			$oSmarty->assign("aArticle", $aArticles[0]);
+			$oSmarty->assign("aQuestion", $aQuestions[0]);
 	} else {
 		if(!empty($aParams["assign"]))
-			$oSmarty->assign($aParams["assign"], $aArticles);
+			$oSmarty->assign($aParams["assign"], $aQuestions);
 		else
-			$oSmarty->assign("aArticles", $aArticles);
+			$oSmarty->assign("aQuestions", $aQuestions);
 	}
 }
