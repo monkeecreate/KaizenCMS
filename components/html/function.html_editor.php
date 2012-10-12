a86c94d45d03d1f24aa8492ad0f324c4f9bf1bd3
James Fleeting <twofivethreetwo@gmail.com>
2012-10-12 15:56:51 -0500

    Updated tinymce, playing with some different skins, changed editor to do inlinepopups now (finally).

--- components/html/function.html_editor.php
+++ components/html/function.html_editor.php
@@ -4,27 +4,27 @@ function smarty_function_html_editor($aParams, &$smarty) {
 		$width = "100%";
 	else
 		$width = $aParams["width"];
-		
+
 	if(empty($params["height"]))
 		$height = "500";
 	else
 		$height = $aParams["height"];
-	
+
 	$content = stripslashes($aParams["content"]);
-	
+
 	$return = "@@@SMARTY:FOOTER:BEGIN@@@\n";
-	$return .= "<script>if(typeof(tinymce) === 'undefined') { document.write('<script src=\"/js/tiny_mce/tiny_mce.js\"><\/script>'); }</script>\n";
+	$return .= "<script>if(typeof(tinymce) === 'undefined') { document.write('<script src=\"/js/tiny_mce/jquery.tinymce.js\"><\/script><script src=\"/js/tiny_mce/tiny_mce.js\"><\/script>'); }</script>\n";
 	$return .= "<script type='text/javascript' src='/js/tiny_mce/plugins/tinybrowser/tb_tinymce.js.php'></script>\n";
 	$return .= "<script type='text/javascript'>\n";
 	$return .= "tinyMCE.init({\n";
-	
+
 	if($_COOKIE[$aParams["name"]."_editor"] == "html")
 		$return .= "\tmode : 'none',\n";
 	else
 		$return .= "\tmode : 'textareas',\n";
 	$return .= "\ttheme : 'advanced',\n";
 	$return .= "\tskin : 'default',\n";
-	$return .= "\tplugins : 'safari,contextmenu,advlist,embed,imagemanager,filemanager,advimage,advlink,paste,table,preview,fullscreen,searchreplace,spellchecker,autolink,tabfocus',\n";
+	$return .= "\tplugins : 'safari,contextmenu,advlist,embed,imagemanager,filemanager,advimage,advlink,paste,table,preview,fullscreen,searchreplace,spellchecker,autolink,tabfocus,inlinepopups',\n";
 	$return .= "\teditor_selector : '".$aParams["name"]."_editor',\n";
 	$return .= "\textended_valid_elements : 'object[width|height|classid|codebase]"
 		.",param[name|value],embed[src|type|width|height|flashvars|wmode]"
@@ -34,7 +34,7 @@ function smarty_function_html_editor($aParams, &$smarty) {
 	$return .= "\twidth: '".$width."',\n";
 	$return .= "\theight: '".$height."',\n";
 	$return .= "\tcontent_css: '/css/editor.css',\n";
-	
+
 	if($aParams["theme"] == "simple") {
 		$return .= "\ttheme_advanced_buttons1 : 'pastetext,pasteword,|,bold,italic,underline,strikethrough,|,numlist,bullist,|,link,unlink,|,undo,redo',\n";
 		$return .= "\ttheme_advanced_buttons2 : '',\n";
@@ -68,9 +68,9 @@ function smarty_function_html_editor($aParams, &$smarty) {
 	$return .= "\t<label class=\"control-label pull-left\" for=\"".$aParams["name"]."_editor\">".$aParams["label"]."</label>\n";
 	$return .= "\t<ul class=\"nav nav-tabs pull-right tinymce-toggle\" style=\"margin-bottom: 0; border-bottom: 0;\"><li class=\"visual_tab active\"><a href=\"javascript:toggleEditorVisual('".$aParams["name"]."_editor');\">Visual</a></li><li class=\"html_tab\"><a href=\"javascript:toggleEditorHTML('".$aParams["name"]."_editor');\">HTML</a></li></ul>\n";
 	$return .= "\t<div class=\"controls\">\n";
-	$return .= "<div id=\"tinymce_editor_".$aParams["name"]."\" class=\"tinymce_editor\" style=\"clear: both;\">\n";	
+	$return .= "<div id=\"tinymce_editor_".$aParams["name"]."\" class=\"tinymce_editor\" style=\"clear: both;\">\n";
 	$return .= "\t<textarea id='".$aParams["name"]."_editor' name='".$aParams["name"]."' class='".$aParams["name"]."_editor full ".$aParams["class"]."' style=\"width: 98.5%; height: ".($height - 15)."px;\">".$content."</textarea><br>\n";
 	$return .= "</div></div>\n";
-	
+
 	return $return;
 }
